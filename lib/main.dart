c8978679b99f797a67b96fc80e8111bec128fcf0
Eria <c121680118@gmail.com>
2019-12-27 05:03:46 +0800

    add

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:countdown_flutter/countdown_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
-import 'package:analog_clock/analog_clock.dart';
+import 'package:flutter_analog_clock/flutter_analog_clock.dart';
+import 'package:flutter/widgets.dart';
+import 'package:time/time.dart';
 
 void main() => runApp(MyApp());
+var onoff = 0;
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -24,11 +27,18 @@ class maitest extends StatelessWidget {
         body: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                test_time(),
-                //timeal(),
-              ],
+            SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlutterAnalogClock(
+                    height: 250,
+                    width: 250,
+                  ),
+                  test_time(),
+                  test_time2(),
+                ],
+              ),
             ),
           ],
         ),
@@ -48,7 +58,7 @@ class _test_timeState extends State<test_time> {
     interval: Duration(seconds: 1),
     duration: Duration(hours: 0),
     builder: (BuildContext ctx, remaining) {
-      return Text(remaining.toString());
+      return Text("距離睡眠時間");
     },
   );
   @override
@@ -57,29 +67,39 @@ class _test_timeState extends State<test_time> {
       child: Column(
         children: <Widget>[
           OutlineButton(
+            child: Text("睡眠時間"),
             onPressed: () async {
               var time = await showTimePicker(
                   context: context, initialTime: TimeOfDay.now());
 
-              var nowt = DateTime.now().hour;
+              var nowt = DateTime.now().hour * 60;
+              nowt += DateTime.now().minute;
+
               var t = DateTimeField.tryFormat(
                   DateTimeField.convert(time), DateFormat("HH"));
+              var all_time = int.parse(t) * 60;
+              t = DateTimeField.tryFormat(
+                  DateTimeField.convert(time), DateFormat("mm"));
+              all_time += int.parse(t);
               var tnumb = nowt - int.parse(t);
               print(nowt);
-              print(t);
-              if (nowt >= int.parse(t)) {
-                tnumb = nowt - int.parse(t) + 24;
+              print(all_time);
+              if (nowt >= all_time) {
+                tnumb = nowt - all_time + (24 * 60);
               } else {
-                tnumb = int.parse(t) - nowt;
+                tnumb = all_time - nowt;
               }
+
               print(tnumb);
+              onoff = 1;
               setState(() {
                 show_time = DateTime.now();
                 a = Countdown(
                   key: Key(Random().nextInt(1000).toString()),
-                  duration: Duration(hours: tnumb),
+                  duration: Duration(minutes: tnumb - 10),
                   builder: (BuildContext ctx, remaining) {
-                    return Text(remaining.toString()); // 01:00:00
+                    return Text(
+                        "距離睡眠還有${remaining.inHours}小時:${remaining.inMinutes % 60}分鐘:${remaining.inSeconds % 60}秒"); // 01:00:00
                   },
                   onFinish: () {},
                 );
@@ -93,25 +113,89 @@ class _test_timeState extends State<test_time> {
   }
 }
 
-class timeal extends StatelessWidget {
+class test_time2 extends StatefulWidget {
+  @override
+  _test_time2State createState() => _test_time2State();
+}
+
+class _test_time2State extends State<test_time2> {
+  DateTime show_time;
+  var a = Countdown(
+    interval: Duration(seconds: 1),
+    duration: Duration(hours: 0),
+    builder: (BuildContext ctx, remaining) {
+      return Text("距離起床時間");
+    },
+  );
   @override
   Widget build(BuildContext context) {
-    return AnalogClock(
-      decoration: BoxDecoration(
-          border: Border.all(width: 2.0, color: Colors.black),
-          color: Colors.transparent,
-          shape: BoxShape.circle),
-      width: 150.0,
-      isLive: true,
-      hourHandColor: Colors.black,
-      minuteHandColor: Colors.black,
-      showSecondHand: false,
-      numberColor: Colors.black87,
-      showNumbers: true,
-      textScaleFactor: 1.4,
-      showTicks: false,
-      showDigitalClock: false,
-      datetime: DateTime(2019, 1, 1, 9, 12, 15),
+    return Container(
+      child: Column(
+        children: <Widget>[
+          OutlineButton(
+            child: Text("起床時間"),
+            onPressed: () async {
+              if (onoff == 1) {
+                var time = await showTimePicker(
+                    context: context, initialTime: TimeOfDay.now());
+                var nowt = DateTime.now().hour * 60;
+                nowt += DateTime.now().minute;
+
+                var t = DateTimeField.tryFormat(
+                    DateTimeField.convert(time), DateFormat("HH"));
+                var all_time = int.parse(t) * 60;
+                t = DateTimeField.tryFormat(
+                    DateTimeField.convert(time), DateFormat("mm"));
+                all_time += int.parse(t);
+                var tnumb = nowt - int.parse(t);
+                print(nowt);
+                print(all_time);
+                if (nowt >= all_time) {
+                  tnumb = nowt - all_time + (24 * 60);
+                } else {
+                  tnumb = all_time - nowt;
+                }
+
+                setState(() {
+                  show_time = DateTime.now();
+                  a = Countdown(
+                    key: Key(Random().nextInt(1000).toString()),
+                    duration: Duration(minutes: tnumb),
+                    builder: (BuildContext ctx, remaining) {
+                      return Text(
+                          "距離起床還有${remaining.inHours}小時:${remaining.inMinutes % 60}分鐘:${remaining.inSeconds % 60}秒"); // 01:00:00
+                    },
+                    onFinish: () {},
+                  );
+                });
+              } else {
+                showAlert1(context);
+              }
+            },
+          ),
+          a,
+        ],
+      ),
     );
   }
+}
+
+Future<void> showAlert1(BuildContext context) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('錯誤'),
+        content: const Text('請先輸入睡眠'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('好 !'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
